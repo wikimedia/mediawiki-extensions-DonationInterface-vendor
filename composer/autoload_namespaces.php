1771e9ee9d5c9d4a2b0378bcc079ba9b29784ef1
Elliott Eggleston <ejegg@ejegg.com>
2016-07-18 15:43:14 -0400

    Update libs
    Change-Id: I59f050bb433c77efb6fff99494f442bafb045e17

--- composer/autoload_namespaces.php
+++ composer/autoload_namespaces.php
@@ -9,5 +9,6 @@ return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'PHPQueue' => array($vendorDir . '/coderkungfu/php-queue/src'),
     'FuseSource' => array($vendorDir . '/fusesource/stomp-php/src'),
+    'ForceUTF8\\' => array($vendorDir . '/neitanod/forceutf8/src'),
     'Clio' => array($vendorDir . '/clio/clio/src'),
 );
